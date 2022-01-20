23bbdf0bc78b04825a53c68f038cb90d0470bfe0
yash-2k21 <yash.sharma_cs20@gla.ac.in>
2022-01-20 23:05:17 +0530

    Leetcode problem

---
+++ src/Problem1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] nums = new int[2*n];
+        for(int i=0 ; i<arr.length ; i++ )
+        {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0 ; i<2*arr.length; i++ )
+        {
+            if(i < arr.length )
+            {
+                nums[i] = arr[i];
+            }
+            else
+            {
+                nums[i] = arr[i-n];
+            }
+        }
+        System.out.println("The Array is - "+ Arrays.toString(arr));
+        System.out.println("The Next Array is - "+ Arrays.toString(nums));
+    }
+}
